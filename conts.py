5c165ed371ad8f0ac69920549667379d9e59fded
prielsmay2007-rgb <prielsmay2007@gmail.com>
2025-08-21 11:10:17 +0300

    Add files via upload

---
+++ conts.py
@@ -0,0 +1,18 @@
+# game grid - MAIN
+GRID_ROW = 25
+GRID_COL = 50
+GRID_COLOR =(138,201,38)
+GRASS_PIC ="grass.png"
+
+#game grid - מוקשים
+MINES_GRID_ROW = 25
+MINES_GRID_COL = 50
+MINES_GRID_COLOR =(0,0,0)
+MINES_GRID_LINES_COLOR = "dark green"
+MINES_HEIGHT = 1
+MINES_LENGTH = 3
+
+
+
+
+
---
+++ screen.py
@@ -0,0 +1,62 @@
+# import conts
+# #פתיחת חלון ראשי
+# # Example file showing a basic pygame "game loop"
+import pygame
+
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((1270,1820))
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # fill the screen with a color to wipe away anything from last frame
+    from conts import GRID_COLOR
+    screen.fill(GRID_COLOR)
+
+    # RENDER YOUR GAME HERE
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    clock.tick(60)  # limits FPS to 60
+
+pygame.quit()
+
+
+
+#םתיחת חלון מוקשים
+# Example file showing a basic pygame "game loop"
+import pygame
+
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # fill the screen with a color to wipe away anything from last frame
+    from conts import MINES_GRID_COLOR
+    screen.fill(MINES_GRID_COLOR)
+
+    # RENDER YOUR GAME HERE
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    clock.tick(60)  # limits FPS to 60
+
+pygame.quit()
