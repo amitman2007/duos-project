ba3a926a167defad62c2ce1ae0e14590d0cc6230
prielsmay2007-rgb <prielsmay2007@gmail.com>
2025-08-24 11:23:21 +0300

    Add files via upload

--- screen.py
+++ screen.py
@@ -1,10 +1,15 @@
 import pygame
 from consts import main_screen
-from duo_project.consts import GRID_COLOR
-from duo_project.soldier import soldier_image, Soldier
+from consts import GRID_COLOR
+# from soldier import soldier_image, Soldier
+#
+#
+# def draw_window(soldier1):
+#     main_screen.fill(GRID_COLOR)
+#     main_screen.blit(Soldier,(soldier1.x,soldier1.y))
+#     pygame.display.update()
 
-
-def draw_window(soldier1):
+def draw_grass(mine):
     main_screen.fill(GRID_COLOR)
-    main_screen.blit(Soldier,(soldier1.x,soldier1.y))
+    main_screen.blit(mine,(mine1))
     pygame.display.update()
